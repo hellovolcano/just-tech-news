f01a16033e0d7027fe2169c396c05d5d9ec27a5b
hellovolcano <valerie.gleason@gmail.com>
2022-08-02 17:51:45 -0500

    adding edit and delete post functionality

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const { Post, User, Vote, Comment } = require('../../models')
 const sequelize = require('../../config/connection')
+const withAuth = require('../../utils/auth')
 
 // get all users
 router.get('/', (req, res) => {
@@ -68,11 +69,11 @@ router.get('/:id', (req, res) => {
     })
 })
 
-router.post('/', (req,res) => {
+router.post('/', withAuth, (req,res) => {
     Post.create({
         title: req.body.title,
         post_url: req.body.post_url,
-        user_id: req.body.user_id
+        user_id: req.session.user_id
     })
     .then(dbPostData => res.json(dbPostData))
     .catch(err => {
@@ -82,7 +83,7 @@ router.post('/', (req,res) => {
 })
 
 // PUT /api/posts/upvote
-router.put('/upvote', (req, res) => {
+router.put('/upvote', withAuth, (req, res) => {
     // make sure the session exists first
     if (req.session) {
         // pass session id along with all destructured properties on req.body
@@ -96,7 +97,7 @@ router.put('/upvote', (req, res) => {
 })
 
 // Update the title of the post
-router.put('/:id', (req,res) => {
+router.put('/:id', withAuth, (req,res) => {
     Post.update(
         {
             title: req.body.title
@@ -123,7 +124,7 @@ router.put('/:id', (req,res) => {
 
 
 // Delete a post
-router.delete('/:id', (req,res) => {
+router.delete('/:id', withAuth, (req,res) => {
     Post.destroy({
         where: {
             id: req.params.id
